4f7d79457bd60b0c8753f628b06bf10bd941fa77
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-05-11 18:20:43 +0300

    Add exercise 06.2

---
+++ Exercise_06.2.py
@@ -0,0 +1,10 @@
+nimi = str(input("Anna nimi: "))
+
+if nimi == "Erkki":
+    salasana = input("Anna salasana: ")
+    if salasana == "Esimerkki":
+        print("Salasana ja nimi oli oikein!")
+    else:
+        print("Salasana oli väärin.")
+else:
+    print("Nimi oli väärin.")
