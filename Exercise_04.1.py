0ace91a87f6b5ff728bbce23e6f939c5af244e49
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-05-11 17:30:54 +0300

    Add exercise 04.1

---
+++ Exercise_04.1.py
@@ -0,0 +1,8 @@
+numero_muuttuja = 10.6411
+merkkijono_muuttuja = "Merkkijono"
+
+numero_muuttuja = int(numero_muuttuja)
+merkkijono_muuttuja = merkkijono_muuttuja * 2
+
+print("Kokonaislukumuunnos ei osaa pyöristää:", numero_muuttuja)
+print("Merkkijonon kertominen tuottaa toistoa:", merkkijono_muuttuja)
