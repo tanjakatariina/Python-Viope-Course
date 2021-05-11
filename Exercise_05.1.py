d4ca1f05c5f08e30311f88012cd3274772cffbeb
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-05-11 17:35:13 +0300

    Add exercise 05.1

---
+++ Exercise_05.1.py
@@ -0,0 +1,9 @@
+muuttuja = "Hattukauppias"
+
+leikkaus_1 = muuttuja[:4]
+leikkaus_2 = muuttuja[-4:]
+leikkaus_3 = muuttuja[::-1]
+
+print("Muuttujan 4 ensimmäistä kirjainta ovat", leikkaus_1)
+print("Muuttujan 4 viimeistä kirjainta ovat", leikkaus_2)
+print("Muuttujan teksti on väärinpäin", leikkaus_3)
