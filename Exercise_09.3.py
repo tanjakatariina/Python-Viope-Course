129f003eb705482f427cc333cbf3b06504e57aab
Tanja Pyykönen <tanja.k.pyykonen@gmail.com>
2021-05-19 13:08:36 +0300

    Add exercise assignments to the comments

--- Exercise_09.3.py
+++ Exercise_09.3.py
@@ -1,3 +1,19 @@
+# Luvun kolmas tehtävä muokkaa eteenpäin luvun ensimmäistä tehtävää. Tällä kertaa tulostaja-funktion toimintaa
+# muutetaan siten, että se vastaanottaa yhden parametrin, jonka oletusarvona on "Oletustulostus" ja tulostaa
+# saamansa arvon. Pääfunktio taas pyytää käyttäjältä syötettä "Anna syöte (Lopeta lopettaa): ", ja mikäli käyttäjä
+# antaa syötteen, joka on 5 tai useampi merkkiä, lähetetään se tulostaja-alifunktiolle. Muussa tapauksessa
+# alifunktiota kutsutaan ilman parametriä. Jos käyttäjä antaa syötteen "Lopeta", ohjelma sammuu.
+
+# Ohjelma toimii seuraavalla tavalla:
+
+# Anna syöte (Lopeta lopettaa): Pitkäsana
+# Pitkäsana
+# Anna syöte (Lopeta lopettaa): möh
+# Oletustulostus
+# Anna syöte (Lopeta lopettaa): Toinenkovinpitkäsana
+# Toinenkovinpitkäsana
+# Anna syöte (Lopeta lopettaa): Lopeta
+
 def main():
     while True:
         syote = input("Anna syöte (Lopeta lopettaa): ")
